04e52ae7d13e6ad4be3abde1448c2b4cc2e9bda8
vernellmiller <nezeey@gmail.com>
2022-02-02 10:55:53 -0500

    assisgnment 1

---
+++ 01-Fundamentals-Part-1/starter/js/assignments.js
@@ -0,0 +1,16 @@
+ /* 
+
+LECTURE: Values and Variables
+
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+
+2. Log their values to the console
+
+*/
+
+let country = "United States of America";
+let continent = "North America";
+let population = 364.29;
+
+console.log(`I live on the continent of ${continent} in the country ${country}. It has a population of ${population} in million. Where do you live?`);

95cce5bf2b15d7286e3bbcfe7d8fcb2414da33d7
vernellmiller <nezeey@gmail.com>
2022-02-02 16:37:58 -0500

    assignment 3 and 4

--- 01-Fundamentals-Part-1/starter/js/assignments.js
+++ 01-Fundamentals-Part-1/starter/js/assignments.js
@@ -37,4 +37,56 @@ console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
 
+/* 
+
+LECTURE: let, const and var
+
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+
+2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+3. Try to change one of the changed variables now, and observe what happens
+
+*/
+
+language = "English";
+
+
+
+
+
+/* 
+
+LECTURE: Basic Operators
+
+1. If your country split in half, and each half would contain half the population,
+then how many people would live in each half?
+
+2. Increase the population of your country by 1 and log the result to the console
+3. Finland has a population of 6 million. Does your country have more people than
+Finland?
+
+4. The average population of a country is 33 million people. Does your country
+have less people than the average country?
+
+5. Based on the variables you created, create a new variable 'description'
+which contains a string with this format: 'Portugal is in Europe, and its 11 million
+people speak portuguese'
+
+*/
+let halfPop = population / 2;
+let addPop = population + 1;
+let findlandPop = 1.0;
+const averageCountryPop = 33.0;
+const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`;
+
+console.log(`${halfPop} in millions is half the population of ${country}`);
+console.log(addPop);
+console.log(population > findlandPop);
+console.log(findlandPop > population);
+console.log(population > averageCountryPop);
+console.log(description);
+
+
 

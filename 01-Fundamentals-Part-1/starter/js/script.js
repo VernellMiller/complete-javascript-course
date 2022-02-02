18a7d26a29e4b2c03fd70d8111000aee7326bf30
vernellmiller <nezeey@gmail.com>
2022-02-02 10:26:26 -0500

    wip: console logging

--- 01-Fundamentals-Part-1/starter/js/script.js
+++ 01-Fundamentals-Part-1/starter/js/script.js
@@ -1,3 +1,10 @@
 let js = "cool";
 
 if (js === "cool") alert("js is super fun, haha...");
+
+console.log(28 * 20);
+
+console.log("Alexandria");
+
+let girlFriend = "Alexandria";
+console.log(girlFriend);

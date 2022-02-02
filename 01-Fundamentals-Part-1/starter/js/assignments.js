6ca4f6cbf68064ca6b271767686e9352cade311c
vernellmiller <nezeey@gmail.com>
2022-02-02 15:16:06 -0500

    assignment 2

--- 01-Fundamentals-Part-1/starter/js/assignments.js
+++ 01-Fundamentals-Part-1/starter/js/assignments.js
@@ -14,3 +14,27 @@ let continent = "North America";
 let population = 364.29;
 
 console.log(`I live on the continent of ${continent} in the country ${country}. It has a population of ${population} in million. Where do you live?`);
+
+/* 
+
+LECTURE: Data Types
+
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+
+*/
+
+let isIsland = "False";
+let language;
+
+console.log(typeof true);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+

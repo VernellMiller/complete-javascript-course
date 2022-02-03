016cd8bf783f7f8a2edcd1e0d65e9876da8151be
vernellmiller <nezeey@gmail.com>
2022-02-03 16:55:50 -0500

    assignment 7

--- 01-Fundamentals-Part-1/starter/js/assignments.js
+++ 01-Fundamentals-Part-1/starter/js/assignments.js
@@ -122,4 +122,35 @@ if (country > averageCountryPop) {
     console.log(`${country}'s population is ${(averageCountryPop - population)} million below average`);
 };
 
-console.log("-------------------");   
+console.log("-------------------");
+
+console.log("Assignment 7");
+console.log("-------------------");
+/*
+
+LECTURE: Type Conversion and Coercion
+
+1. Predict the result of these 5 operations without executing them:
+'9' - '5';
+'19' - '13' + '17';
+'19' - '13' + 17;
+'123' < 57;
+5 + 6 + '4' + 9 - 4 - 2;
+
+2. Execute the operations to check if you were right
+
+*/
+
+// line 1 will log - 4
+// line 2 will log - 23
+// line 3 will log - 11
+// line 4 will log - false
+// line 5 will log - 16
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+console.log("-------------------");

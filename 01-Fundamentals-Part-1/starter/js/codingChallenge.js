af87a9293d881851b67b9d6df20f9b1d3b3edc42
vernellmiller <nezeey@gmail.com>
2022-02-08 15:46:41 -0500

    coding challenge 4

--- 01-Fundamentals-Part-1/starter/js/codingChallenge.js
+++ 01-Fundamentals-Part-1/starter/js/codingChallenge.js
@@ -133,3 +133,46 @@ if (dolphinsAverage > koalasAverage) {
 }
 
 console.log("-------------------");
+
+console.log("-------------------");
+console.log("Coding Challenge 4");
+
+/*
+
+Coding Challenge #4
+
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+
+    1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+
+    2. Print a string to the console containing the bill value, the tip, and the final value (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+
+    Test data: § Data 1: Test for bill values 275, 40 and 430
+
+    Hints: § To calculate 20% of a value, simply multiply it by 20/100 = 0.2 § Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+
+GOOD LUCK 😀
+
+*/
+
+// let tip;
+// const billValue = 275;
+
+// const bill = billValue >= 50 && billValue <= 300 ? `The Bill Value:${billValue}, The Tip Value:${tip = .15} and the Final Value: ${tip * billValue} Total: ${billValue + tip}` : `The Bill Value:${billValue}, The Tip Value:${tip = .20} and the Final Value: ${tip + billValue} Total: ${billValue + tip}`; 
+
+// console.log(tip);
+// console.log(bill);
+
+const billValue = 275;
+let tip = billValue >= 50 && billValue <= 300 ? billValue * .15 : billValue * .20;
+console.log(`The Bill Value:${billValue}, The Tip Value: ${tip} and the Final Value: ${tip + billValue}`);
+
+// const bill = 275;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// console.log(tip);
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
+console.log("-------------------");

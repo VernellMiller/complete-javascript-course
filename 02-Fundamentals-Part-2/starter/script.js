5a873d0c2e9e766d0e4aecd1546c986b18ef8c46
vernellmiller <nezeey@gmail.com>
2022-02-17 13:55:41 -0500

    fix: assignment 10

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -265,7 +265,12 @@ const myCountry = {
     neighbours: [neighbours],
 
     describe: function() {
-        console.log(`${this.country} has ${this.population} million people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`);
+        return `${this.country} has ${this.population} million people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`;
+    },
+
+    checkIsland: function() {
+        this.isIsland = true ? neighbours === 0 : this.isIsland = false;
+        return this.isIsland;
     },
 }
 
@@ -321,9 +326,8 @@ LECTURE: Object Methods
 
 */
 
-
-
 console.log(myCountry.describe());
+console.log(myCountry.checkIsland());
 
 console.log("---------------------|");
 

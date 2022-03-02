b7044a094d5c981a8bcb74068e41861b3991cc01
vernellmiller <nezeey@gmail.com>
2022-03-02 15:38:00 -0500

    coding challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,33 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]} C in ${arr.indexOf([i]) + 1} days.`);
+
+    for (let value = 0; value < arr.length; value++) {
+      console.log(`${arr[i]} C in ${value + 1} days.`);
+    }
+    return;
+  }
+};
+
+console.log("Test Data 1:");
+console.log("-------------");
+printForecast([17, 21, 23]);
+console.log("-------------");
+console.log("Test Data 2:");
+console.log("-------------");
+printForecast([12, 5, -5, 0, 4]);
+
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+
+// const printForecast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}ºC in ${i + 1} days ... `;
+//   }
+//   console.log('...' + str);
+// };
+// printForecast(data1);

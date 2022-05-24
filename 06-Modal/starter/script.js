183c12dd9e68dc7b0a6fd2f7dc0e3c696dc9aa07
vernellmiller <nezeey@gmail.com>
2022-05-24 11:40:42 -0400

    selects classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const modal = document.getElementsByClassName('modal');
+const overlay = document.getElementsByClassName('overlay');
+const btnCloseModal = document.getElementsByClassName('close-modal');
+const btnsOpenModal = document.getElementsByClassName('show-modal');
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}

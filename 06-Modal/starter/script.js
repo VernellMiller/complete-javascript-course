08de325c876b756804ce3d1b9964152123f357e0
vernellmiller <nezeey@gmail.com>
2022-05-24 13:59:19 -0400

    opens and closes modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,12 +1,24 @@
 'use strict';
 
-const modal = document.getElementsByClassName('modal');
-const overlay = document.getElementsByClassName('overlay');
-const btnCloseModal = document.getElementsByClassName('close-modal');
-const btnsOpenModal = document.getElementsByClassName('show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
